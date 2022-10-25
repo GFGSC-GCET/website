901a591a7de77e1ae1acd8f8faa57acc82efdf70
areeburrub <areeburrub@gmail.com>
2022-10-26 01:40:22 +0530

    fix: file name fix

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,4 @@ export { default as Slides } from "./eventSlides";
 export { default as ThemeChanger } from "./themeToggle";
 export { default as ContactForm } from "./contactForm";
 export { default as Footer } from "./footer"
-export { default as Faq } from "./faq"
+export { default as Faq } from "./FAQ"
