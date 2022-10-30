596addcd59f72ba2201c9a56f19ad5e1d155fde3
areeburrub <areeburrub@gmail.com>
2022-10-30 16:04:08 +0530

    added: breadcrumbs

---
+++ src/components/breadcrumbs/index.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+const Breadcrumbs = () => {
+  const router = useRouter();
+  const url = router.pathname;
+  
+  const [breadcrumbs, setBreadcrumbs] = useState([]);
+  const [breadcrumbsPath, setBreadcrumbsPath] = useState([]);
+
+  useEffect(() => {
+    const crumbs = url.split("/");
+    crumbs.shift();
+    setBreadcrumbs(crumbs);
+
+    const crumbsPath = [];
+    crumbs.forEach((crumb, index) => {
+      crumbsPath.push(crumbs.slice(0, index + 1).join("/"));
+    });
+    setBreadcrumbsPath(crumbsPath);
+  }, [url]);
+
+  useEffect(() => {
+    console.log(breadcrumbs);
+  }, [breadcrumbs]);
+
+  return (
+    <div className="">
+      <div className="container flex items-center px-6 py-4 mx-auto overflow-y-auto whitespace-nowrap">
+        <Link href="/" >
+          <a className="text-gray-600 dark:text-gray-200">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-5 h-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              >
+              <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+            </svg>
+          </a>
+        </Link>
+
+
+        {breadcrumbs.map((crumb, index) => {
+          return (
+            <span className='flex items-center' key={index}>
+              <span className="mx-5 text-gray-500 dark:text-gray-300 rtl:-scale-x-100">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-5 h-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </span>
+
+              <Link href={`/${breadcrumbsPath[index]}`}>
+                <a className="text-gray-600 dark:text-gray-200 hover:underline">
+                  {crumb}
+                </a>
+              </Link>
+            </span>
+            );
+          })}
+        </div>
+
+    </div>
+  );
+}
+
+export default Breadcrumbs
