4c25a582c3d9600ca03fed482961ec79c56fe734
areeburrub <areeburrub@gmail.com>
2022-11-02 01:58:26 +0530

    added: Select Field in Registration Form

---
+++ src/components/miscs/Form/selectField.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Field, ErrorMessage, useField } from "formik";
+
+const SelectField = ({ label, disabled, spanClass,options, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <div className={spanClass}>
+      <label className="text-sm">{label}</label>
+      {/* create a select dropdown*/}
+      <Field
+        as='select'
+        {...field}
+        className={`w-full bg-gray-200 dark:bg-gray-700 px-3 py-2 rounded-md focus:ring focus:ring-opacity-75 focus:ring-green-400 dark:border-gray-700 dark:text-gray-100 outline-0 disabled:cursor-not-allowed`}
+        disabled={disabled}
+      >
+        {/* create options for the select dropdown*/}
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </Field>
+
+      <ErrorMessage
+        name={field.name}
+        component="div"
+        className="text-red-500 text-xs"
+      />
+    </div>
+  );
+};
+
+export default SelectField;
