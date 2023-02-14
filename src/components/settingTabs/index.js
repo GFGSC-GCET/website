a0d1e4e39f78a2c1ea1274a2ecd8192ef1764652
areeburrub <areeburrub@gmail.com>
2023-02-14 14:32:56 +0530

    added: event manage page in settings

--- src/components/settingTabs/index.js
+++ src/components/settingTabs/index.js
@@ -1,4 +1,5 @@
 export {default as ProfileTab } from './profileTab'
 export {default as TeamTab } from './teamTab'
 export {default as EventTab } from './eventTab'
+
 export {default as ManageEventTab } from './manageEventTab'
